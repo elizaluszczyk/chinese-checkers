538a3a3fcfd2596f4572fbb6d8626ce185d0e1cf
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2024-12-15 16:14:58 +0100

    Delete trylma/src/main/java/com/example/trylma/interfaces/Pawn.java

--- trylma/src/main/java/com/example/trylma/interfaces/Pawn.java
+++
@@ -1,9 +0,0 @@
-package com.example.trylma.interfaces;
-
-public interface Pawn {
-        String getOwner();
-        void setOwner(String owner);
-        String getColor();
-        boolean isActive();
-        void deactivate();
-}
