2476c7c86135035dfaf425924d82ab931243f967
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2024-12-15 16:14:27 +0100

    Update Player.java

--- trylma/src/main/java/com/example/trylma/interfaces/Player.java
+++ trylma/src/main/java/com/example/trylma/interfaces/Player.java
@@ -2,8 +2,4 @@ package com.example.trylma.interfaces;
 
 public interface Player {
     String getUsername();
-    String getColor();
-    void setColor(String color);
-    boolean isActive();
-    void deactivate();
 }
