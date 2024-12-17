2c8eb7de56311b271bb7e1a9de27f204394b8250
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2024-12-17 18:59:16 +0100

    Create Move.java

---
+++ trylma/src/main/java/com/example/trylma/board/Move.java
@@ -0,0 +1,31 @@
+package com.example.trylma.game;
+
+public class Move {
+    private final int startX;
+    private final int startY;
+    private final int endX;
+    private final int endY;
+
+    public Move(int startX, int startY, int endX, int endY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+
+    public int getStartX() {
+        return startX;
+    }
+
+    public int getStartY() {
+        return startY;
+    }
+
+    public int getEndX() {
+        return endX;
+    }
+
+    public int getEndY() {
+        return endY;
+    }
+}
