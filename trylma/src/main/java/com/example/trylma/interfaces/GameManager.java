e58bd713cac10c9212f74760321dea6404dd403a
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2024-12-17 19:07:44 +0100

    Remove helping classes

--- trylma/src/main/java/com/example/trylma/interfaces/GameManager.java
+++
@@ -1,5 +0,0 @@
-package com.example.trylma.interfaces;
-
-public interface GameManager {
-    Board getBoard();
-}
--- trylma/src/main/java/com/example/trylma/interfaces/MoveParser.java
+++
@@ -1,8 +0,0 @@
-package com.example.trylma.interfaces;
-
-import com.example.trylma.exceptions.InvalidMoveException;
-import com.example.trylma.game.Move;
-
-public interface MoveParser {
-    Move parseMove(String message) throws InvalidMoveException;
-}
