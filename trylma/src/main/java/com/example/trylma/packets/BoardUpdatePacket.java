44cb89287963e9047f4366bbcf1cb648f430a7e0
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-05 13:55:43 +0100

    Add serial version uid

--- trylma/src/main/java/com/example/trylma/packets/BoardUpdatePacket.java
+++ trylma/src/main/java/com/example/trylma/packets/BoardUpdatePacket.java
@@ -6,6 +6,7 @@ import com.example.trylma.interfaces.Board;
 import com.example.trylma.server.ServerPacket;
 
 public class BoardUpdatePacket implements ServerPacket {
+    private static final long serialVersionUID = 1L;
     private final Board board;
     private final ArrayList<String> movedPerformedByPlayers;
 
--- trylma/src/main/java/com/example/trylma/packets/TextMessagePacket.java
+++ trylma/src/main/java/com/example/trylma/packets/TextMessagePacket.java
@@ -3,6 +3,7 @@ package com.example.trylma.packets;
 import com.example.trylma.server.ServerPacket;
 
 public class TextMessagePacket implements ServerPacket {
+    private static final long serialVersionUID = 1L;
     private final String messageString;
 
     public TextMessagePacket(String messageString) {
