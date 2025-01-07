707030364e08b43183c71bb8c4f77e334666cda8
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-07 21:05:50 +0100

    Add custom invalid game settings exception

---
+++ trylma/src/main/java/com/example/trylma/exceptions/InvalidGameSettingsException.java
@@ -0,0 +1,7 @@
+package com.example.trylma.exceptions;
+
+public class InvalidGameSettingsException extends Exception {
+    public InvalidGameSettingsException(String message) {
+        super(message);
+    }
+}
