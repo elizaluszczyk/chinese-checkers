34ee19bd995b1145cc9eb6c6e49d6299e0c506df
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-09 20:42:10 +0100

    Remove list with moves performed by players

--- trylma/src/main/java/com/example/trylma/packets/BoardUpdatePacket.java
+++ trylma/src/main/java/com/example/trylma/packets/BoardUpdatePacket.java
@@ -1,25 +1,17 @@
 package com.example.trylma.packets;
 
-import java.util.ArrayList;
-
 import com.example.trylma.interfaces.Board;
 import com.example.trylma.server.ServerPacket;
 
 public class BoardUpdatePacket implements ServerPacket {
     private static final long serialVersionUID = 1L;
     private final Board board;
-    private final ArrayList<String> movedPerformedByPlayers;
 
     public BoardUpdatePacket(Board board) {
         this.board = board;
-        this.movedPerformedByPlayers = new ArrayList<>(board.getMovesPerformedByPlayers());
     }
 
     public Board getBoard() {
         return board;
-    }
-
-    public ArrayList<String> getMovedPerformedByPlayers() {
-        return movedPerformedByPlayers;
     }
 }
