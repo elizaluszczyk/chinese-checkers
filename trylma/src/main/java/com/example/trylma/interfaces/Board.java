16ec186b4ceb28843fd3538b6bfb34120ca78c48
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-09 20:46:28 +0100

    Remove unnecessary methods

--- trylma/src/main/java/com/example/trylma/interfaces/Board.java
+++ trylma/src/main/java/com/example/trylma/interfaces/Board.java
@@ -2,13 +2,7 @@ package com.example.trylma.interfaces;
 
 import java.util.ArrayList;
 
-import com.example.trylma.board.Move;
-import com.example.trylma.board.Pawn;
-
 public interface Board {
-    ArrayList<ArrayList<Pawn>> getBoard();
-    void addMoveTakenByPlayer(Player player, Move move);
-    boolean isInBoard(int x, int y);
-    ArrayList<String> getMovesPerformedByPlayers();
-
+    ArrayList<ArrayList<Field>> getBoard();
+    Field getField(int x, int y);
 }
