398b3397687ef815ac6858812ca60c2c03195fd4
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-13 20:47:53 +0100

    Add enum with game types

---
+++ trylma/src/main/java/com/example/trylma/game/GameType.java
@@ -0,0 +1,14 @@
+package com.example.trylma.game;
+
+public enum GameType {
+    DEFAULT;
+
+    public static boolean isValid(String input) {
+        for (GameType variant : GameType.values()) {
+            if (variant.name().equalsIgnoreCase(input)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
