f00c367db81603eb3f81eee1df4f4f87919a85be
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-19 02:47:06 +0100

    Add field data wrapper

---
+++ trylma/src/main/java/com/example/trylma/packets/FieldData.java
@@ -0,0 +1,52 @@
+package com.example.trylma.packets;
+
+import java.io.Serializable;
+
+public class FieldData implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int x;
+    private final int y;
+    private final boolean active;
+    private final boolean occupied;
+    private final String ownerId;
+
+    public FieldData(int x, int y, boolean active, boolean occupied, String ownerId) {
+        this.x = x;
+        this.y = y;
+        this.active = active;
+        this.occupied = occupied;
+        this.ownerId = ownerId;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+    
+    public boolean isOccupied() {
+        return occupied;
+    }
+    
+    @Override
+    public String toString() {
+        if (!active) {
+            return " ";
+        } else if (!occupied){
+            return "*";
+        } else {
+            return "@";
+        }
+    }
+}
+
