a7e139067fedbc573e75451145997376441e9dd2
Eliza Łuszczyk <eliza.luszczyk@gmail.com>
2025-01-29 00:20:07 +0100

    Add bot player with possible moves

---
+++ trylma/src/main/java/com/example/trylma/game/BotPlayer.java
@@ -0,0 +1,74 @@
+package com.example.trylma.game;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.example.trylma.board.ChineseCheckersBoard;
+import com.example.trylma.board.Move;
+import com.example.trylma.board.Pawn;
+import com.example.trylma.interfaces.Field;
+
+public class BotPlayer extends GamePlayer {
+    private final Random random = new Random();
+
+    public BotPlayer(String username) {
+        super(username);
+    }
+
+    public Move generateMove(ChineseCheckersBoard board) {
+        ArrayList<Move> possibleMoves = getPossibleNeighbourFieldsMove(board);
+        System.out.println("Possible moves: " + possibleMoves);
+
+        if (possibleMoves.isEmpty()) {
+            return null; 
+        }
+
+        int index = random.nextInt(possibleMoves.size());
+        return possibleMoves.get(index);
+    }
+
+    private ArrayList<Move> getPossibleNeighbourFieldsMove(ChineseCheckersBoard board) {
+        ArrayList<Move> possibleMoves = new ArrayList<>();
+
+        for (Pawn pawn : this.getPawns()) {
+            Field currentField = pawn.getCurrentField();
+
+            ArrayList<Move> movesForCurrentField = generateMovesForField(board, currentField);
+            System.out.println(("For field " + currentField.getX() + ", " + currentField.getY() + " are possible moves: " + movesForCurrentField));
+
+            
+            for (Move move : movesForCurrentField) {
+                    possibleMoves.add(move);
+            }
+            
+        }
+
+        return possibleMoves;
+    }
+
+    private ArrayList<Move> generateMovesForField(ChineseCheckersBoard board, Field field) {
+        ArrayList<Move> moves = new ArrayList<>();
+
+        int[][] directions = {
+            {1, -1}, 
+            {-1, -1}, 
+            {2, 0},
+            {-1, 1}, 
+            {1, 1},
+            {-2, 0}  
+        };
+
+        for (int[] direction : directions) {
+            int targetX = field.getX() + direction[0];
+            int targetY = field.getY() + direction[1];
+
+            Field targetField = board.getField(targetY, targetX);
+            if (targetField != null && !targetField.isOccupied() && targetField.isActive()) {
+                Move possibleMove = new Move(field.getX(), field.getY(), targetX, targetY);
+                moves.add(possibleMove);
+            }
+        }
+
+        return moves;
+    }
+}
