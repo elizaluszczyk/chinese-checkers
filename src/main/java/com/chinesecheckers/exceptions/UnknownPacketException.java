fe9178fe7bc8c1905d31453711c95758b4dd2f92
eliza <eliza.luszczyk@gmail.com>
2025-02-05 15:50:01 +0200

    Add custom UnknownPacketException

---
+++ src/main/java/com/chinesecheckers/exceptions/UnknownPacketException.java
@@ -0,0 +1,7 @@
+package com.chinesecheckers.exceptions;
+
+public class UnknownPacketException extends Exception {
+    public UnknownPacketException(String message) {
+        super(message);
+    }
+}
