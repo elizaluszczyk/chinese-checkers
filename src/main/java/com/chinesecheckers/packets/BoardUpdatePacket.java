4aca6c3afab096fa2637053f4af85cf5c245cf9b
eliza <eliza.luszczyk@gmail.com>
2025-02-06 16:33:48 +0200

    Remove unused method and fix style of code

--- src/main/java/com/chinesecheckers/packets/BoardUpdatePacket.java
+++ src/main/java/com/chinesecheckers/packets/BoardUpdatePacket.java
@@ -16,7 +16,6 @@ public class BoardUpdatePacket implements ServerPacket {
             this.board.add(new ArrayList<>());
             for (Field field : row) {
                 
-                
                 String playerId = null;
 
                 if (field.isOccupied()) playerId = field.getPawn().getPlayerId();
@@ -34,19 +33,6 @@ public class BoardUpdatePacket implements ServerPacket {
 
     public ArrayList<ArrayList<FieldData>> getBoard() {
         return board;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (ArrayList<FieldData> row : board) {
-            for (FieldData field : row) {
-                builder.append(field.isActive() ? field.toString() : " ");
-            }
-            builder.append("\n");
-        }
-        builder.append("]");
-        return builder.toString();
     }
 
     @Override
