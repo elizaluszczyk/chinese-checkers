3f931060e1104a12e26a983dbdfdb9d10741cb1e
eliza <eliza.luszczyk@gmail.com>
2025-02-06 15:20:57 +0200

    Add default configuration constants

--- src/main/java/com/chinesecheckers/server/GameManagerSingleton.java
+++ src/main/java/com/chinesecheckers/server/GameManagerSingleton.java
@@ -6,11 +6,14 @@ import com.chinesecheckers.interfaces.GameManager;
 public class GameManagerSingleton {
     private static GameManager instance;
 
+    private static final String DEFAULT_GAME_TYPE = "default";
+    private static final int DEFAULT_PLAYER_COUNT = 2;
+
     private GameManagerSingleton() {}
 
     public static synchronized GameManager getInstance() {
         if (instance == null) {
-            instance = new StandardGameManager("default", 2);
+            instance = new StandardGameManager(DEFAULT_GAME_TYPE, DEFAULT_PLAYER_COUNT);
         }
         return instance;
     }
