ced0329090fd57961434079c30d609960e13ba1c
eliza <eliza.luszczyk@gmail.com>
2025-07-17 15:35:18 +0200

    Exctract random position selection logic to dedicated method for better readbility, reuse setPosition helper method

--- src/main/java/com/chinesecheckers/board/YinAndYangBoard.java
+++ src/main/java/com/chinesecheckers/board/YinAndYangBoard.java
@@ -3,6 +3,7 @@ package com.chinesecheckers.board;
 import java.util.Random;
 
 public class YinAndYangBoard extends DefaultBoardWithPlacedPawns {
+    private final Random random = new Random();
 
     public YinAndYangBoard(int numberOfPlayers) {
         super(numberOfPlayers);
@@ -12,23 +13,21 @@ public class YinAndYangBoard extends DefaultBoardWithPlacedPawns {
     public void setStartingAndTargetPositions(int numberOfPlayers) {
         if (listOfPlayers.size() < numberOfPlayers) return;
 
-        switch (numberOfPlayers) {
-            case 2 -> {
-                Random rand = new Random();
-
-                int randomStarArm1 = rand.nextInt(6);
-                int randomStarArm2;
-
-                do {
-                    randomStarArm2 = rand.nextInt(6);
-                } while (randomStarArm2 == randomStarArm1);
-
-                listOfPlayers.get(0).setStartingPositions(getArmsOfStar().get(randomStarArm1));
-                listOfPlayers.get(0).setTargetPositions(getArmsOfStar().get(randomStarArm2));
-                listOfPlayers.get(1).setStartingPositions(getArmsOfStar().get(randomStarArm2));
-                listOfPlayers.get(1).setTargetPositions(getArmsOfStar().get(randomStarArm1));
-            }
-            default -> throw new IllegalArgumentException("Invalid number of players: " + numberOfPlayers);
+        if (numberOfPlayers == 2) {
+            setRandomOppositePositions();
+        } else {
+            throw new IllegalArgumentException("Invalid number of players: " + numberOfPlayers);
         }
+    }
+    
+    private void setRandomOppositePositions() {
+        int arm1 = random.nextInt(6);
+        int arm2;
+        do {
+            arm2 = random.nextInt(6);
+        } while (arm2 == arm1);
+        
+        setPositions(0, arm1, arm2);
+        setPositions(1, arm2, arm1);
     }
 }
